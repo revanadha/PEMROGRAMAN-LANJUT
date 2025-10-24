be7a450defc877c861064f401e454f55b1816d20
revanadha <revansa888@gmail.com>
2025-10-24 12:48:37 +0800

    commit

--- MODUL 3/src/Main.java
+++ MODUL 3/src/Main.java
@@ -1,7 +1,9 @@
-// File: src/Main.java
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Selamat datang di DocumentationPractice!");
         System.out.println("Program ini menampilkan pesan sederhana untuk latihan dokumentasi Git dan Java.");
     }
 }
+
+
